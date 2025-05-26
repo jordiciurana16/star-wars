33ffab470951960ee820e05871f18d811b46238d
jordiciurana16 <jciuranas@gmail.com>
2025-05-26 11:14:14 +0200

    email verification feedback ui

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DatabaseService } from '../../../service/database.service';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,16 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private databaseService: DatabaseService) {}
+  constructor(
+    private databaseService: DatabaseService,
+    private router: Router
+  ) {}
 
   async onLogin() {
     try {
       const data = await this.databaseService.logIn(this.email, this.password);
       alert('User logged in successfully:' + data);
+      this.router.navigate(['/starships']);
     } catch (error) {
       alert('Error logging in:' + error);
     }
--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { DatabaseService } from '../../../service/database.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
 })
 export class SignUpComponent {
   email: string = '';
   password: string = '';
+  emailFeedback: boolean = false;
 
   constructor(private databaseService: DatabaseService) {}
 
   async onSignUp() {
     try {
       const user = await this.databaseService.signUp(this.email, this.password);
-      console.log('User signed up successfully:', user);
+      alert('User signed up successfully:' + user);
+      this.emailFeedback = true;
     } catch (error) {
-      console.error('Error signing up:', error);
+      alert('Error signing up:' + error);
     }
   }
 }
